10fbb62591f284057d073c94987f97b24cb3baf7
James Buncle <jbuncle@hotmail.com>
2022-11-28 12:48:17 +0000

    Enabled GITLAB_OMNIBUS_CONFIG env var

--- assets/gitlab.rb
+++ assets/gitlab.rb
@@ -21,7 +21,7 @@ nginx['client_max_body_size'] = '2000M'
 # Load custom config from environment variable: GITLAB_OMNIBUS_CONFIG
 # Disabling the cop since rubocop considers using eval to be security risk but
 # we don't have an easy way out, atleast yet.
-#eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s # rubocop:disable Security/Eval
+eval ENV["GITLAB_OMNIBUS_CONFIG"].to_s # rubocop:disable Security/Eval
 
 # Load configuration stored in /etc/gitlab/gitlab.rb
 #from_file("/etc/gitlab/gitlab.rb")
